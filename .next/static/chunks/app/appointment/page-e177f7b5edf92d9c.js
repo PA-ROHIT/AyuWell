e161d9f497768f684e8694cc5551aa0c01f18502
Rohit <entrybyrohit@gmail.com>
2025-08-08 11:47:56 +0530

    Fix JSX unescaped entities and Mail import

---
+++ .next/static/chunks/app/appointment/page-e177f7b5edf92d9c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[536,786],{3859:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[619,441,258,358],()=>e(e.s=3859)),_N_E=e.O()}]);
